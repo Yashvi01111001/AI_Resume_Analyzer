743ac903b5dd1192770483d47abf41d8fade765d
Yashvi01111001 <yashvijayesh@gmail.com>
2025-07-31 16:37:53 +0530

    latest commits

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return(
         <nav className="navbar">
             <Link to="/">
-                <p className="text-2xl font-bold text-gradient">MATCHMUSE</p>
+                <p className="text-2xl font-bold text-gradient">DEEPCEPT AI</p>
             </Link>
             <Link to = "/upload" className="primary-button w-fit">
                 Upload resume 📝
